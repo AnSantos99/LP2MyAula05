b8a4465f7f2ea160c5a79b93e88f1656aa4ab157
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-10-23 15:31:48 +0100

    TryCatch Exceptions

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -6,12 +6,22 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
-            
+            // Imprimir informacao
             Console.WriteLine("Insere um numero inteiro");
 
-            int i = Convert.ToInt32(Console.ReadLine());
+            // Tenta correr este bloco
+            try
+            {
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Numero inserido : {i}");
+            }
 
-            Console.WriteLine($"Numero inserido : {i}");
+            // Apanha ecepcoes se algo for diferrente
+            catch (Exception a) 
+            {
+                Console.WriteLine("Ocorreu o seguinte problema" + a.Message);
+            }
+            
         }
     }
 }
