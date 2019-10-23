62e4a17b8c5aba5993239b34b7c9c85b23c81f93
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-10-23 15:12:10 +0100

    Exeptest

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Insere um numero inteiro");
+
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Numero inserido : {i}");
+        }
+    }
+}
